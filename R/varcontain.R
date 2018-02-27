5b94f65a642ee4087347b3d80a3d76ba363b9f9b
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2018-02-27 15:15:44 -0500

    Add varcontain.

---
+++ R/varcontain.R
@@ -0,0 +1,19 @@
+#' varcontain
+#'
+#' This function returns any variable names in a given data set that contain
+#' the a provided string. The
+#' @param df A dataframe
+#' @param x A string that the returned variables in the dataframe will contain
+#' @ignorecase Ignore case of the string? Default is TRUE
+#' @keywords variable names
+#' @export
+#' @examples
+#' tdf <- data.frame(x=4,t=6,e=2,b=4,yep=9,Nope=10,cort1=3,cort4=5,Cort_55=9)
+#' varcontain(tdf,'cort')
+#' [1] "cort1"   "cort4"   "Cort_55"
+
+varcontain <- function(df, x, ignorecase=TRUE) {
+  colnames(df)[grepl(x,
+                     colnames(df),
+                     ignore.case = ignorecase)]
+}
