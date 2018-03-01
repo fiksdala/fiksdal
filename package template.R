2596bafdeba78515921484956450b83cafa08dca
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2018-03-01 10:22:45 -0500

    update package template

--- package template.R
+++ package template.R
@@ -1,20 +1,7 @@
-setwd("/Users/alex/Documents/R")
-library("devtools")
-library(roxygen2)
-
-create("fiksdal")
-setwd("./fiksdal")
-document()
-setwd("..")
-install("fiksdal")
-library(fiksdal)
-
-
 # Update fiksdal package
 library("devtools")
 library(roxygen2)
 setwd("/Users/alex/Documents/R/fiksdal")
 document()
-setwd("..")
-install("fiksdal")
+install_github('fiksdala/fiksdal')
 library(fiksdal)
