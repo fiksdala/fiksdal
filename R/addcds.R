cf19e296604d230b4fb5b8eaf32d8752432bf78f
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2018-03-02 11:52:31 -0500

    update addcds for different ID names

--- R/addcds.R
+++ R/addcds.R
@@ -1,18 +1,19 @@
 #' addcds
 #'
 #' This function returns a dataframe with cook's distance stats obtained through
-#' influence.ME's influence() function
+#' influence.ME's influence() function.
 #' @param df dataframe
 #' @param influence.object An object returned from influence.ME's influence()
+#' @param ID ID name of df
 #' @keywords influence, cook's d, influence.ME
 #' @export
 #' @examples
 #' addcds()
 
-addcds <- function(df,influence.object) {
+addcds <- function(df,influence.object,ID) {
   cds <- cooks.distance(influence.object)
-  cdf <- data.frame(ID=rownames(cds),
+  cdf <- data.frame(paste0(ID)=rownames(cds),
                     cd=cds)
-  df <- merge(df,cdf,by='ID')
+  df <- merge(df,cdf,by=ID)
   df
 }
