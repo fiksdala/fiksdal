4927044cd08138705cf77d2fb6e3453e8a107be8
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2018-03-03 13:03:20 -0500

    add except to varcontain

--- R/varcontain.R
+++ R/varcontain.R
@@ -3,7 +3,10 @@
 #' This function returns any variable names in a given data set that contain
 #' the a provided string.
 #' @param df A dataframe
-#' @param x A string that the returned variables in the dataframe will contain
+#' @param matching A string that the returned variables in the dataframe will
+#' contain
+#' @param except A string that the returned variables in the dataframe will NOT
+#' contain
 #' @param ignorecase Ignore case of the string? Default is TRUE
 #' @keywords variable names
 #' @export
@@ -12,8 +15,16 @@
 #' varcontain(tdf,'cort')
 #' [1] "cort1"   "cort4"   "Cort_55"
 
-varcontain <- function(df, x, ignorecase=TRUE) {
-  colnames(df)[grepl(x,
-                     colnames(df),
-                     ignore.case = ignorecase)]
+varcontain <- function(df, matching='all',
+                       except='none', ignorecase=TRUE) {
+<<<<<<< HEAD
+  x <- ifelse(matching=='all','',matching)
+=======
+  x <- ifelse(matching=='all',colnames(df),matching)
+>>>>>>> b53fb3772c8244afd87a705f4f533e8715a053dd
+  y <- ifelse(except=='none',runif(1))
+  output <- colnames(df)[grepl(x,
+                               colnames(df),
+                               ignore.case = ignorecase)]
+  output <- output[!grepl(y,output,ignore.case=ignorecase)]
 }
