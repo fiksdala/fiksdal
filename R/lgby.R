2bbec7a46f7cc101705aa8ba8de04d6085b20071
Alexander Fiksdal <alexander.fiksdal@gmail.com>
2018-03-07 11:56:23 -0500

    add error_bars to lgby

--- R/lgby.R
+++ R/lgby.R
@@ -1,19 +1,22 @@
 #' lgby
 #'
-#' This function quickly executes the aggregate and ggplot functions to produce 
+#' This function quickly executes the aggregate and ggplot functions to produce
 #' a simple line graph with separate lines based on an indicated group.
 #' @param df A dataframe.
-#' @param x The variable to appear on the x-axis. 
-#' @param y The variable to appear on the y-axis. 
+#' @param x The variable to appear on the x-axis.
+#' @param y The variable to appear on the y-axis.
 #' @param by The grouping variable.
-#' @param facet_var The facet-grouping variable. 
-#' @param grid_var The grid-grouping variable. When using grid_var, it is essentially interchangable with facet_var, as together they determine the order and axis of the grid. grid_var cannot be used independently of facet_var for obvious reasons.
+#' @param facet_var The facet-grouping variable.
+#' @param grid_var The grid-grouping variable. When using grid_var, it is
+#' essentially interchangable with facet_var, as together they determine the
+#' order and axis of the grid. grid_var cannot be used independently of
+#' facet_var for obvious reasons.
 #' @keywords line graph
 #' @export
 #' @examples
 #' lgby()
 
-lgby <- function(df, x, y, by, facet_var, grid_var) {
+lgby <- function(df, x, y, by, facet_var, grid_var,error_bars=TRUE) {
     if(missing(facet_var)==TRUE & missing(grid_var)==FALSE) {
         stop("grid_var only allowed in conjunction with facet_var")}
     vars <- c(x, y)
@@ -24,34 +27,36 @@ lgby <- function(df, x, y, by, facet_var, grid_var) {
     s.names <- names(dfin)[-2]
     dfin.names <- c("x", "y", "by", "facet_var", "grid_var")
     names(dfin) <- dfin.names[1:length(dfin)]
-    
+
     bylist.df <- dfin[-2]
     bylist <- list()
     for(i in 1:length(bylist.df)) {
         bylist[length(bylist)+1] <- bylist.df[i]
     }
-    
+
     dfout <- aggregate(dfin$y, bylist, mean)
-    names(dfout) <- c(s.names, y)
-    
-    if(missing(by)==TRUE & missing(facet_var)==TRUE & missing(grid_var)==TRUE) 
+    ses <- aggregate(dfin$y,bylist,SEM)
+    dfout <- cbind(dfout,ses[,-1])
+    names(dfout) <- c(s.names, 'y','se')
+
+    if(missing(by)==TRUE & missing(facet_var)==TRUE & missing(grid_var)==TRUE)
     {output <- ggplot(dfout, aes_string(x=colnames(dfout)[1],
                                         y=colnames(dfout)[2])) +
         geom_line()
     }
-    if(missing(by)==FALSE & missing(facet_var)==TRUE & missing(grid_var)==TRUE) 
+    if(missing(by)==FALSE & missing(facet_var)==TRUE & missing(grid_var)==TRUE)
     {output <- ggplot(dfout, aes_string(x=colnames(dfout)[1],
                                         y=colnames(dfout)[3],
                                         color=colnames(dfout)[2])) +
         geom_line()
     }
-    if(missing(by)==TRUE & missing(facet_var)==FALSE & missing(grid_var)==TRUE) 
+    if(missing(by)==TRUE & missing(facet_var)==FALSE & missing(grid_var)==TRUE)
     {output <- ggplot(dfout, aes_string(x=colnames(dfout)[1],
                                         y=colnames(dfout)[3])) +
         geom_line() +
         facet_wrap(as.formula(paste("~", colnames(dfout)[2])))
     }
-    if(missing(by)==FALSE & missing(facet_var)==FALSE & 
+    if(missing(by)==FALSE & missing(facet_var)==FALSE &
        missing(grid_var)==TRUE) {
         output <- ggplot(dfout, aes_string(x=colnames(dfout)[1],
                                            y=colnames(dfout)[4],
@@ -59,15 +64,21 @@ lgby <- function(df, x, y, by, facet_var, grid_var) {
             geom_line() +
             facet_wrap(as.formula(paste("~", colnames(dfout)[3])))
     }
-    if(missing(by)==FALSE & missing(facet_var)==FALSE & 
+    if(missing(by)==FALSE & missing(facet_var)==FALSE &
        missing(grid_var)==FALSE) {
         output <- ggplot(dfout, aes_string(x=colnames(dfout)[1],
                                            y=colnames(dfout)[5],
                                            color=colnames(dfout)[2])) +
             geom_line() +
             facet_grid(as.formula(paste(colnames(dfout)[3],
-                                        "~", 
+                                        "~",
                                         colnames(dfout)[4])))
     }
+    if(error_bars==TRUE) {
+      output <- output +
+        geom_errorbar(aes(ymin=y-se, ymax=y+se), width=.1)
+    }
+    output <- output +
+      ylab(y)
     output
 }
